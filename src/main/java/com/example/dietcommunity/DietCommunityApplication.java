90a115c388e7d79b01541152ea03fde24f3254cb
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2023-11-16 20:03:51 +0900

    first commit

---
+++ src/main/java/com/example/dietcommunity/DietCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietCommunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DietCommunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/dietcommunity/DietCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietCommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
